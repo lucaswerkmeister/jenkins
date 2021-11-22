81debb359915f8f7421f865062463d142f35574a
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2021-11-22 19:14:50 +0000

    Cleanup

--- war/src/main/js/config-scrollspy.js
+++ war/src/main/js/config-scrollspy.js
@@ -1,27 +1,25 @@
 import $ from 'jquery'
 import page from './util/page'
 
-// Some stuff useful for testing.
-export var scrollspeed = window.matchMedia("(prefers-reduced-motion: reduce)").matches ? 0 : 350
+const defaultIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M262.29 192.31a64 64 0 1057.4 57.4 64.13 64.13 0 00-57.4-57.4zM416.39 256a154.34 154.34 0 01-1.53 20.79l45.21 35.46a10.81 10.81 0 012.45 13.75l-42.77 74a10.81 10.81 0 01-13.14 4.59l-44.9-18.08a16.11 16.11 0 00-15.17 1.75A164.48 164.48 0 01325 400.8a15.94 15.94 0 00-8.82 12.14l-6.73 47.89a11.08 11.08 0 01-10.68 9.17h-85.54a11.11 11.11 0 01-10.69-8.87l-6.72-47.82a16.07 16.07 0 00-9-12.22 155.3 155.3 0 01-21.46-12.57 16 16 0 00-15.11-1.71l-44.89 18.07a10.81 10.81 0 01-13.14-4.58l-42.77-74a10.8 10.8 0 012.45-13.75l38.21-30a16.05 16.05 0 006-14.08c-.36-4.17-.58-8.33-.58-12.5s.21-8.27.58-12.35a16 16 0 00-6.07-13.94l-38.19-30A10.81 10.81 0 0149.48 186l42.77-74a10.81 10.81 0 0113.14-4.59l44.9 18.08a16.11 16.11 0 0015.17-1.75A164.48 164.48 0 01187 111.2a15.94 15.94 0 008.82-12.14l6.73-47.89A11.08 11.08 0 01213.23 42h85.54a11.11 11.11 0 0110.69 8.87l6.72 47.82a16.07 16.07 0 009 12.22 155.3 155.3 0 0121.46 12.57 16 16 0 0015.11 1.71l44.89-18.07a10.81 10.81 0 0113.14 4.58l42.77 74a10.8 10.8 0 01-2.45 13.75l-38.21 30a16.05 16.05 0 00-6.05 14.08c.33 4.14.55 8.3.55 12.47z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>`
 
-// Used to set scrollspeed from the the test suite
-export function setScrollspeed(newScrollspeed) {
-    scrollspeed = newScrollspeed
-}
+// Some stuff useful for testing.
+export var scrollSpeed = window.matchMedia("(prefers-reduced-motion: reduce)").matches ? 0 : 350
 
 $(function() {
   const sideNav = document.querySelector(".jenkins-side-nav")
   const headers = document.querySelectorAll("h2, .config-table > .jenkins-section > .jenkins-section__title")
 
+  // Create the sidebar links
   headers.forEach(function(header, i) {
     const textContent = header.textContent.replaceAll(" ", "")
     header.id = textContent
 
     let item = document.createElement("button")
     item.className = "jenkins-side-nav__item"
-    item.innerHTML = header.textContent
+    item.innerHTML = "<div>" + defaultIcon + "</div>" + header.textContent
     item.setAttribute("section-id", textContent)
-    item.addEventListener("click", function (e) {
+    item.addEventListener("click", function () {
       const headerToGoTo = document.querySelector("#" + textContent)
       scrollTo(i === 0 ? 0 : (headerToGoTo.getBoundingClientRect().top + window.scrollY) - 40 - 30)
     })
@@ -38,7 +36,7 @@ $(function() {
 function scrollTo(yPosition) {
   $('html,body').animate({
     scrollTop: yPosition
-  }, scrollspeed)
+  }, scrollSpeed)
 }
 
 /**
@@ -50,7 +48,7 @@ function autoActivateTabs() {
 
   let selectedSection = null
 
-  // calculate the top and height of each section to know where to switch the tabs...
+  // Calculate the top and height of each section to know where to switch the tabs
   sections.forEach(function (section, i) {
     const previousSection = i === 1 ? document.querySelector(".jenkins-section:first-of-type") : sections[Math.max(i - 1, 0)].parentNode
     const viewportEntryOffset = i === 0 ? 0 : ((section.parentNode.getBoundingClientRect().top + window.scrollY) - (previousSection.offsetHeight / 2))
