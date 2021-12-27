f9e75dea17a9af3183555740c1ce10843f68564f
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2021-12-27 21:37:50 +0000

    Cleanup, add animations, use vars and rems

--- war/src/main/js/command-centre.js
+++ war/src/main/js/command-centre.js
@@ -6,6 +6,7 @@ const spotlightButton = document.getElementById("button-spotlight")
 const commandCentreBackdrop = document.getElementById("command-centre-backdrop")
 const commandCentre = document.getElementById("command-centre")
 const commandBarInput = document.getElementById("command-bar")
+const commandBarMagnifyingGlass = commandCentre.querySelector(".jenkins-command-centre__search .icon")
 const searchResults = document.getElementById("search-results")
 const searchResultsContainer = document.getElementById("search-results-container")
 const keyboardModifier = document.getElementById("command-centre-shortcut-modifier")
@@ -16,141 +17,168 @@ const hoverClass = "jenkins-command-centre__results__item--hover"
 const isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0
 keyboardModifier.textContent = isMac ? "⌘" : "CTRL"
 
-spotlightButton.addEventListener("click", function() {
-    showCommandCentre()
+spotlightButton.addEventListener("click", function () {
+  showCommandCentre()
 })
 
-commandCentre.addEventListener("click", function(e) {
+commandCentre.addEventListener("click", function (e) {
   if (e.target !== e.currentTarget) {
-    return;
+    return
   }
 
   hideCommandCentre()
 })
 
-hotkeys('ctrl+k, command+k', async function() {
-    showCommandCentre()
-    // Returning false stops the event and prevents default browser events
-    return false
+hotkeys('ctrl+k, command+k', async function () {
+  showCommandCentre()
+  // Returning false stops the event and prevents default browser events
+  return false
 })
 
 function showCommandCentre() {
-    commandCentre.style.display = "flex"
-    commandCentreBackdrop.style.display = "block"
-    commandBarInput.focus()
+  commandCentre.style.display = "flex"
+  commandCentreBackdrop.style.display = "block"
+  commandBarInput.focus()
+
+  // Fire empty input event to command bar to set appropriate UI states (OOBE, results, no results)
+  commandBarInput.dispatchEvent(new Event('input'))
 }
 
 function hideCommandCentre() {
-    commandCentre.style.display = "none"
-    commandCentreBackdrop.style.display = "none"
+  commandCentre.style.display = "none"
+  commandCentreBackdrop.style.display = "none"
 }
 
 function groupByKey(array, key) {
-    return array
-        .reduce((hash, obj) => {
-            if(obj[key] === undefined) return hash
-            return Object.assign(hash, { [obj[key]]:( hash[obj[key]] || [] ).concat(obj)})
-        }, {})
+  return array
+    .reduce((hash, obj) => {
+      if (obj[key] === undefined) return hash
+      return Object.assign(hash, {[obj[key]]: (hash[obj[key]] || []).concat(obj)})
+    }, {})
 }
 
 commandBarInput.addEventListener('input', async function (e) {
+  commandBarMagnifyingGlass.classList.add("icon--loading")
+
+  if (e.target.value.length === 0) {
+    generateOobe()
+  } else {
     let response = await fetch('http://localhost:8080/jenkins/search/suggest?query=' + e.target.value)
     let result = await response.json()
 
     // Clear current search results
     searchResults.innerHTML = ''
 
-    // Group the suggestions
-    const groupedSuggestions = groupByKey(result["suggestions"], "group")
+    if (result["suggestions"].length > 0) {
+      // Group the suggestions
+      const groupedSuggestions = groupByKey(result["suggestions"], "group")
 
-    for (const [group, items] of Object.entries(groupedSuggestions)) {
+      for (const [group, items] of Object.entries(groupedSuggestions)) {
         const heading = document.createElement('p')
         heading.className = "jenkins-command-centre__results__heading"
         heading.innerText = group
         searchResults.append(heading)
 
-        items.forEach(function(obj) {
-            let link = document.createElement('DIV')
-            link.innerHTML = `<a class="jenkins-command-centre__results__item" href="${obj.url}">
-                            <div class="jenkins-command-centre__results__item__icon">${obj.icon ? `${obj.icon.svg ? obj.icon.svg : `<img src="${obj.icon.url}" alt="" />`}` : ``}</div>
-                            ${obj.name}
-                            ${obj.description ? `<span class="jenkins-command-centre__results__item__description">${obj.description}</span>` : ''}
-                            <svg class="jenkins-command-centre__results__item__chevron" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M184 112l144 144-144 144"/></svg>
-                        </a>`
-            link = link.firstChild
-            link.addEventListener("mouseenter", e => itemMouseEnter(e))
-            searchResults.append(link)
+        items.forEach(function (obj) {
+          let link = document.createElement('DIV')
+          link.innerHTML = `<a class="jenkins-command-centre__results__item" href="${obj.url}">
+                              <div class="jenkins-command-centre__results__item__icon">${obj.icon ? `${obj.icon.svg ? obj.icon.svg : `<img src="${obj.icon.url}" alt="" />`}` : ``}</div>
+                              ${obj.name}
+                              ${obj.description ? `<span class="jenkins-command-centre__results__item__description">${obj.description}</span>` : ''}
+                              <svg class="jenkins-command-centre__results__item__chevron" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M184 112l144 144-144 144"/></svg>
+                          </a>`
+          link = link.firstChild
+          link.addEventListener("mouseenter", e => itemMouseEnter(e))
+          searchResults.append(link)
         })
+      }
+
+      updateSelectedItem(0)
+    } else {
+      const heading = document.createElement('p')
+      heading.className = "jenkins-command-centre__results__heading"
+      heading.innerText = "No results for " + e.target.value
+      searchResults.append(heading)
     }
+  }
 
-    updateSelectedItem(0)
-
-    searchResultsContainer.style.height = searchResults.offsetHeight + "px"
+  searchResultsContainer.style.height = searchResults.offsetHeight + "px"
+  commandBarMagnifyingGlass.classList.remove("icon--loading")
 })
 
-commandBarInput.addEventListener("keyup", function(event) {
-    const maxLength = searchResults.getElementsByTagName("a").length
-    let selectedIndex = -1
-    let hoveredItem = document.querySelector("." + hoverClass)
+commandBarInput.addEventListener("keyup", function (event) {
+  const maxLength = searchResults.getElementsByTagName("a").length
+  let selectedIndex = -1
+  let hoveredItem = document.querySelector("." + hoverClass)
 
-    if (hoveredItem) {
-        selectedIndex = [...hoveredItem.parentElement.getElementsByTagName("a")].indexOf(hoveredItem)
-    }
+  if (hoveredItem) {
+    selectedIndex = [...hoveredItem.parentElement.getElementsByTagName("a")].indexOf(hoveredItem)
+  }
 
-    switch (event.code) {
-        case 'Enter':
-            if (hoveredItem) {
-                window.location.href = hoveredItem.href
-            }
-            return false
-        case 'ArrowUp':
-            if (selectedIndex !== -1) {
-                if (selectedIndex - 1 < 0) {
-                    selectedIndex = maxLength - 1
-                } else {
-                    selectedIndex --
-                }
+  switch (event.code) {
+    case 'Enter':
+      if (hoveredItem) {
+        window.location.href = hoveredItem.href
+      }
+      return false
+    case 'ArrowUp':
+      if (selectedIndex !== -1) {
+        if (selectedIndex - 1 < 0) {
+          selectedIndex = maxLength - 1
+        } else {
+          selectedIndex--
+        }
 
-                updateSelectedItem(selectedIndex, selectedIndex + 1 >= maxLength)
-            }
-            return false
-        case 'ArrowDown':
-            if (selectedIndex !== -1) {
-                if (selectedIndex + 1 >= maxLength) {
-                    selectedIndex = 0
-                } else {
-                    selectedIndex ++
-                }
+        updateSelectedItem(selectedIndex, selectedIndex + 1 >= maxLength)
+      }
+      return false
+    case 'ArrowDown':
+      if (selectedIndex !== -1) {
+        if (selectedIndex + 1 >= maxLength) {
+          selectedIndex = 0
+        } else {
+          selectedIndex++
+        }
 
-                updateSelectedItem(selectedIndex, selectedIndex + 1 >= maxLength)
-            }
-            return false
-    }
+        updateSelectedItem(selectedIndex, selectedIndex + 1 >= maxLength)
+      }
+      return false
+  }
 })
 
 function itemMouseEnter(item) {
-    let hoveredItems = document.querySelector("." + hoverClass)
-    if (hoveredItems) {
-        hoveredItems.classList.remove(hoverClass)
-    }
+  let hoveredItems = document.querySelector("." + hoverClass)
+  if (hoveredItems) {
+    hoveredItems.classList.remove(hoverClass)
+  }
 
-    item.target.classList.add(hoverClass)
+  item.target.classList.add(hoverClass)
 }
 
 function updateSelectedItem(index, scrollIntoView = false) {
-    const maxLength = searchResults.getElementsByTagName("a").length
-    const hoveredItem = document.querySelector("." + hoverClass)
+  const maxLength = searchResults.getElementsByTagName("a").length
+  const hoveredItem = document.querySelector("." + hoverClass)
 
-    if (hoveredItem) {
-        hoveredItem.classList.remove(hoverClass)
+  if (hoveredItem) {
+    hoveredItem.classList.remove(hoverClass)
+  }
+
+  if (index < maxLength) {
+    const element = [...searchResults.getElementsByTagName("a")][index]
+    element.classList.add(hoverClass)
+
+    if (scrollIntoView) {
+      element.scrollIntoView()
     }
+  }
+}
 
-    if (index < maxLength) {
-       const element = [...searchResults.getElementsByTagName("a")][index]
-        element.classList.add(hoverClass)
+function generateOobe() {
+  // Clear current search results
+  searchResults.innerHTML = ''
 
-        if (scrollIntoView) {
-            element.scrollIntoView();
-        }
-    }
+  const heading = document.createElement('p')
+  // heading.className = "jenkins-command-centre__results__heading"
+  heading.innerText = "Welcome to the new Jenkins' search"
+  searchResults.append(heading)
 }
