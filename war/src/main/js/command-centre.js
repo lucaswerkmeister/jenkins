85f1afb27667727e60d4d2529086a5a25872e4ba
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2021-12-28 00:08:08 +0000

    Add search help link

--- war/src/main/js/command-centre.js
+++ war/src/main/js/command-centre.js
@@ -64,7 +64,7 @@ commandBarInput.addEventListener('input', async function (e) {
   if (e.target.value.length === 0) {
     generateOobe()
   } else {
-    let response = await fetch('http://localhost:8080/jenkins/search/suggest?query=' + e.target.value)
+    let response = await fetch(document.getElementById("header").dataset.searchUrl.escapeHTML() + '?query=' + e.target.value)
     let result = await response.json()
 
     // Clear current search results
@@ -178,8 +178,56 @@ function generateOobe() {
   // Clear current search results
   searchResults.innerHTML = ''
 
-  const heading = document.createElement('p')
-  // heading.className = "jenkins-command-centre__results__heading"
-  heading.innerText = "Welcome to the new Jenkins' search"
-  searchResults.append(heading)
+  const groupedSuggestions = {
+    "Featured": [
+      {
+        icon: {
+          svg: "<svg xmlns=\"http://www.w3.org/2000/svg\" class=\"ionicon\" viewBox=\"0 0 512 512\"><title>Help Circle</title><path d=\"M256 80a176 176 0 10176 176A176 176 0 00256 80z\" fill=\"none\" stroke=\"currentColor\" stroke-miterlimit=\"10\" stroke-width=\"32\"/><path d=\"M200 202.29s.84-17.5 19.57-32.57C230.68 160.77 244 158.18 256 158c10.93-.14 20.69 1.67 26.53 4.45 10 4.76 29.47 16.38 29.47 41.09 0 26-17 37.81-36.37 50.8S251 281.43 251 296\" fill=\"none\" stroke=\"currentColor\" stroke-linecap=\"round\" stroke-miterlimit=\"10\" stroke-width=\"28\"/><circle cx=\"250\" cy=\"348\" r=\"20\" fill=\"currentColor\"/></svg>"
+        },
+        name: "Get help using Jenkins search",
+        url: document.getElementById("header").dataset.searchHelpUrl.escapeHTML()
+      },
+      {
+        icon: {
+          svg: "<svg xmlns=\"http://www.w3.org/2000/svg\" class=\"ionicon\" viewBox=\"0 0 512 512\"><title>Help Circle</title><path d=\"M256 80a176 176 0 10176 176A176 176 0 00256 80z\" fill=\"none\" stroke=\"currentColor\" stroke-miterlimit=\"10\" stroke-width=\"32\"/><path d=\"M200 202.29s.84-17.5 19.57-32.57C230.68 160.77 244 158.18 256 158c10.93-.14 20.69 1.67 26.53 4.45 10 4.76 29.47 16.38 29.47 41.09 0 26-17 37.81-36.37 50.8S251 281.43 251 296\" fill=\"none\" stroke=\"currentColor\" stroke-linecap=\"round\" stroke-miterlimit=\"10\" stroke-width=\"28\"/><circle cx=\"250\" cy=\"348\" r=\"20\" fill=\"currentColor\"/></svg>"
+        },
+        name: "Get help using Jenkins search",
+        url: document.getElementById("header").dataset.searchHelpUrl.escapeHTML()
+      },
+      {
+        icon: {
+          svg: "<svg xmlns=\"http://www.w3.org/2000/svg\" class=\"ionicon\" viewBox=\"0 0 512 512\"><title>Help Circle</title><path d=\"M256 80a176 176 0 10176 176A176 176 0 00256 80z\" fill=\"none\" stroke=\"currentColor\" stroke-miterlimit=\"10\" stroke-width=\"32\"/><path d=\"M200 202.29s.84-17.5 19.57-32.57C230.68 160.77 244 158.18 256 158c10.93-.14 20.69 1.67 26.53 4.45 10 4.76 29.47 16.38 29.47 41.09 0 26-17 37.81-36.37 50.8S251 281.43 251 296\" fill=\"none\" stroke=\"currentColor\" stroke-linecap=\"round\" stroke-miterlimit=\"10\" stroke-width=\"28\"/><circle cx=\"250\" cy=\"348\" r=\"20\" fill=\"currentColor\"/></svg>"
+        },
+        name: "Get help using Jenkins search",
+        url: document.getElementById("header").dataset.searchHelpUrl.escapeHTML()
+      },
+      {
+        icon: {
+          svg: "<svg xmlns=\"http://www.w3.org/2000/svg\" class=\"ionicon\" viewBox=\"0 0 512 512\"><title>Help Circle</title><path d=\"M256 80a176 176 0 10176 176A176 176 0 00256 80z\" fill=\"none\" stroke=\"currentColor\" stroke-miterlimit=\"10\" stroke-width=\"32\"/><path d=\"M200 202.29s.84-17.5 19.57-32.57C230.68 160.77 244 158.18 256 158c10.93-.14 20.69 1.67 26.53 4.45 10 4.76 29.47 16.38 29.47 41.09 0 26-17 37.81-36.37 50.8S251 281.43 251 296\" fill=\"none\" stroke=\"currentColor\" stroke-linecap=\"round\" stroke-miterlimit=\"10\" stroke-width=\"28\"/><circle cx=\"250\" cy=\"348\" r=\"20\" fill=\"currentColor\"/></svg>"
+        },
+        name: i18n.dataset.getHelp,
+        url: document.getElementById("header").dataset.searchHelpUrl.escapeHTML()
+      }
+    ]
+  }
+
+  for (const [group, items] of Object.entries(groupedSuggestions)) {
+    const heading = document.createElement('p')
+    heading.className = "jenkins-command-centre__results__heading"
+    heading.innerText = group
+    searchResults.append(heading)
+
+    items.forEach(function (obj) {
+      let link = document.createElement('DIV')
+      link.innerHTML = `<a class="jenkins-command-centre__results__item" href="${obj.url}">
+                              <div class="jenkins-command-centre__results__item__icon">${obj.icon ? `${obj.icon.svg ? obj.icon.svg : `<img src="${obj.icon.url}" alt="" />`}` : ``}</div>
+                              ${obj.name}
+                              ${obj.description ? `<span class="jenkins-command-centre__results__item__description">${obj.description}</span>` : ''}
+                              <svg class="jenkins-command-centre__results__item__chevron" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M184 112l144 144-144 144"/></svg>
+                          </a>`
+      link = link.firstChild
+      link.addEventListener("mouseenter", e => itemMouseEnter(e))
+      searchResults.append(link)
+    })
+  }
 }
--- war/src/main/webapp/scripts/hudson-behavior.js
+++ war/src/main/webapp/scripts/hudson-behavior.js
@@ -2024,55 +2024,6 @@ function ensureVisible(e) {
     }
 }
 
-// set up logic behind the search box
-function createSearchBox(searchURL) {
-    var ds = new YAHOO.util.XHRDataSource(searchURL+"suggest");
-    ds.responseType = YAHOO.util.XHRDataSource.TYPE_JSON;
-    ds.responseSchema = {
-        resultsList: "suggestions",
-        fields: ["name"]
-    };
-    var ac = new YAHOO.widget.AutoComplete("search-box","search-box-completion",ds);
-    ac.typeAhead = false;
-    ac.autoHighlight = false;
-    ac.formatResult = ac.formatEscapedResult;
-    ac.maxResultsDisplayed = 25;
-
-    var box   = $("search-box");
-    var sizer = $("search-box-sizer");
-    var comp  = $("search-box-completion");
-
-    Behaviour.addLoadEvent(function(){
-        // copy font style of box to sizer
-        var ds = sizer.style;
-        ds.fontFamily = getStyle(box, "fontFamily");
-        ds.fontSize = getStyle(box, "fontSize");
-        ds.fontStyle = getStyle(box, "fontStyle");
-        ds.fontWeight = getStyle(box, "fontWeight");
-    });
-
-    // update positions and sizes of the components relevant to search
-    function updatePos() {
-        sizer.innerHTML = box.value.escapeHTML();
-        var cssWidth, offsetWidth = sizer.offsetWidth;
-        if (offsetWidth > 0) {
-            cssWidth = offsetWidth + "px";
-        } else { // sizer hidden on small screen, make sure resizing looks OK
-            cssWidth =  getStyle(sizer, "minWidth");
-        }
-        box.style.width =
-        comp.firstElementChild.style.minWidth = "calc(60px + " + cssWidth + ")";
-
-        var pos = YAHOO.util.Dom.getXY(box);
-        pos[1] += YAHOO.util.Dom.get(box).offsetHeight + 2;
-        YAHOO.util.Dom.setXY(comp, pos);
-    }
-
-    updatePos();
-    box.addEventListener("input", updatePos);
-}
-
-
 /**
  * Finds the DOM node of the given DOM node that acts as a parent in the form submission.
  *
