7ac9744218b6402a58bc0f3e891ffcc13085e383
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-03-13 13:18:16 +0000

    We can now switch between types and clone

--- core/src/main/resources/lib/form/dynamicSelect/dynamicSelect.js
+++ core/src/main/resources/lib/form/dynamicSelect/dynamicSelect.js
@@ -1,9 +1,17 @@
-window.addEventListener('load', function () {
+window.addEventListener("load", function () {
   let dynamicDropdowns = document.querySelectorAll(".jenkins-dynamic-select")
   dynamicDropdowns.forEach(function (e) {
     let relatedDropdown = e.nextSibling.childNodes[0]
+
+    // Show and hide the dropdown
     e.addEventListener("click", e => {
-      relatedDropdown.classList.add("jenkins-dynamic-select__items--visible")
+      relatedDropdown.classList.toggle("jenkins-dynamic-select__items--visible")
+    })
+
+    relatedDropdown.querySelectorAll("input[type='radio']").forEach(function(radio) {
+      radio.addEventListener("change", function() {
+        e.innerHTML = radio.nextSibling.innerHTML
+      })
     })
   })
 })
