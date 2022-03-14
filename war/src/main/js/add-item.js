a7c4795b561874ffc6456e31f98d3d89bee7054f
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-03-14 11:36:48 +0000

    Update add-item.js

--- war/src/main/js/add-item.js
+++ war/src/main/js/add-item.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function () {
-  const form = document.querySelector('#createItem')
+  const form = document.querySelector("#createItem")
   const toggleButtons = document.querySelectorAll("#button-project-type, #button-clone")
   const paneProjectType = document.querySelector("#pane-project-type")
   const paneClone = document.querySelector("#pane-clone")
@@ -24,29 +24,29 @@ window.addEventListener("load", function () {
   })
 
   /**
-   * @param {string|undefined} fieldName - Only validate this field if provided, otherwise validate all
+   * @param {string|undefined} fieldName - Only validate this field if provided, otherwise validate all fields
    */
   function validateFormItems(fieldName) {
     const data = Object.fromEntries(new FormData(form).entries())
 
-    const validationNameError = document.querySelector('#validation-name-error')
-    const validationProjectTypeError = document.querySelector('#validation-project-type-error')
-    const validationCloneError = document.querySelector('#validation-clone-error')
+    const validationNameError = document.querySelector("#validation-name-error")
+    const validationProjectTypeError = document.querySelector("#validation-project-type-error")
+    const validationCloneError = document.querySelector("#validation-clone-error")
 
-    if (fieldName === 'name' || !fieldName) {
+    if (fieldName === "name" || !fieldName) {
       validationNameError.classList.toggle("jenkins-hidden", data.name.trim().length !== 0)
     }
-    if (fieldName === 'mode' || !fieldName) {
-      validationProjectTypeError.classList.toggle("jenkins-hidden", 'mode' in data && data.mode !== "copy")
+    if (fieldName === "mode" || !fieldName) {
+      validationProjectTypeError.classList.toggle("jenkins-hidden", "mode" in data && data.mode !== "copy")
     }
-    if (fieldName === 'from' || !fieldName) {
-      validationCloneError.classList.toggle("jenkins-hidden", data.mode === "copy" && 'from' in data)
+    if (fieldName === "from" || !fieldName) {
+      validationCloneError.classList.toggle("jenkins-hidden", data.mode === "copy" && "from" in data)
     }
 
     return [...document.querySelectorAll(".error")].every(e => e.offsetParent === null)
   }
 
-  form.addEventListener('submit', function(event) {
+  form.addEventListener("submit", function(event) {
     if (!validateFormItems(undefined)) {
       event.preventDefault()
     }
