fe06b998add433ad616c68426485890492866084
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-04-16 23:44:03 +0100

    Add dialog

--- core/src/main/resources/lib/layout/confirmationLink/adjunct.js
+++ core/src/main/resources/lib/layout/confirmationLink/adjunct.js
@@ -3,16 +3,22 @@ Behaviour.specify("A.confirmation-link", 'confirmation-link', 0, function (eleme
         var post = element.getAttribute('data-post');
         var href = element.getAttribute('data-url');
         var message = element.getAttribute('data-message');
-        if (confirm(message)) {
-            var form = document.createElement('form');
-            form.setAttribute('method', (post === "true") ? 'POST' : 'GET');
-            form.setAttribute('action', href);
-            if (post) {
-                crumb.appendToForm(form);
-            }
-            document.body.appendChild(form);
-            form.submit();
+
+        const options = {
+          "title": message
         }
+        showConfirmationModal(options)
+
+        // if (confirm(message)) {
+        //     var form = document.createElement('form');
+        //     form.setAttribute('method', (post === "true") ? 'POST' : 'GET');
+        //     form.setAttribute('action', href);
+        //     if (post) {
+        //         crumb.appendToForm(form);
+        //     }
+        //     document.body.appendChild(form);
+        //     form.submit();
+        // }
         return false;
     };
 });
--- war/src/main/js/modals.js
+++ war/src/main/js/modals.js
@@ -1,6 +1,9 @@
 import {createElementFromHtml} from "./util/dom";
 
 const defaults = {
+  background: "var(--modal-background)",
+  padding: "1.5rem",
+  minWidth: undefined,
   maxWidth: undefined,
   hideCloseButton: false
 };
@@ -9,6 +12,9 @@ export function showModal(contents, options = {}) {
   options = { ...defaults, ...options };
   const modal = document.createElement("dialog");
   modal.classList.add("jenkins-modal");
+  modal.style.background = options.background;
+  modal.style.padding = options.padding;
+  modal.style.minWidth = options.minWidth;
   modal.style.maxWidth = options.maxWidth;
   modal.innerHTML = contents;
 
@@ -27,3 +33,32 @@ export function showModal(contents, options = {}) {
 
   modal.showModal();
 }
+
+const confirmationDefaults = {
+  background: "transparent",
+  padding: "0",
+  minWidth: "500px",
+  maxWidth: "525px",
+  hideCloseButton: true
+};
+
+export function showConfirmationModal(options = {}) {
+  options = { ...confirmationDefaults, ...options };
+
+  const html = `
+    <form method="dialog">
+        <div class="longhorn-container">
+            <p class="jenkins-modal__title">${options.title}</p>
+            <p class="jenkins-modal__label">${options.description}</p>
+        </div>
+        <div class="jenkins-buttons-row jenkins-buttons-row--invert jenkins-buttons-row--equal-buttons longhorn-container-2">
+            <button class="jenkins-button jenkins-button--primary jenkins-button--destructive">Yes</button>
+            <button class="jenkins-button">Cancel</button>
+        </div>
+    </form>
+  `
+
+  showModal(html, options)
+}
+
+window.showConfirmationModal = showConfirmationModal
