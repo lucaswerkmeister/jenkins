0a9795614b9bb6ca3ae371287d3da604e2c24801
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-04-27 00:12:20 +0100

    Init

--- war/src/main/js/pages/plugin-manager/available.js
+++ war/src/main/js/pages/plugin-manager/available.js
@@ -1,73 +1,64 @@
-import debounce from 'lodash/debounce'
-import requestAnimationFrame from 'raf';
+import debounce from "lodash/debounce";
 
 import pluginManager from "@/api/pluginManager";
-import pluginManagerAvailable from "@/templates/plugin-manager/available.hbs"
-import {registerRefreshServerButton} from "@/pages/plugin-manager/shared"
+import pluginManagerAvailable from "@/templates/plugin-manager/available.hbs";
 
-const filterInput = document.getElementById('filter-box');
+const searchBar = document.querySelector("#filter-box");
 
 function applyFilter(searchQuery) {
-    // debounce reduces number of server side calls while typing
-    pluginManager.availablePluginsSearch(searchQuery.toLowerCase().trim(), 50, function (plugins) {
-        var pluginsTable = document.getElementById('plugins');
-        var tbody = pluginsTable.querySelector('tbody');
-        var admin = pluginsTable.dataset.hasadmin === 'true';
-        var selectedPlugins = [];
+  // debounce reduces number of server side calls while typing
+  pluginManager.availablePluginsSearch(searchQuery.toLowerCase().trim(), 50, function (plugins) {
+    const pluginsTable = document.getElementById('plugins');
+    const tbody = pluginsTable.querySelector('tbody');
+    const admin = pluginsTable.dataset.hasadmin === 'true';
+    let selectedPlugins = [];
 
-        var filterInput = document.getElementById('filter-box');
-        filterInput.parentElement.classList.remove("app-plugin-manager__search--loading");
+    const filterInput = document.getElementById('filter-box');
+    filterInput.parentElement.classList.remove("app-plugin-manager__search--loading");
 
-        function clearOldResults() {
-            if (!admin) {
-                tbody.innerHTML = '';
+    function clearOldResults() {
+      if (!admin) {
+        tbody.innerHTML = '';
+      } else {
+        const rows = tbody.querySelectorAll('tr');
+        if (rows) {
+          selectedPlugins = []
+          rows.forEach(function (row) {
+            const input = row.querySelector('input');
+            if (input.checked === true) {
+              const pluginName = input.name.split('.')[1];
+              selectedPlugins.push(pluginName)
             } else {
-                var rows = tbody.querySelectorAll('tr');
-                if (rows) {
-                    selectedPlugins = []
-                    rows.forEach(function (row) {
-                        var input = row.querySelector('input');
-                        if (input.checked === true) {
-                            var pluginName = input.name.split('.')[1];
-                            selectedPlugins.push(pluginName)
-                        } else {
-                            row.remove();
-                        }
-                    })
-                }
+              row.remove();
             }
+          })
         }
+      }
+    }
 
-        clearOldResults()
-        var rows = pluginManagerAvailable({
-            plugins: plugins.filter(plugin => selectedPlugins.indexOf(plugin.name) === -1),
-            admin
-        });
-
-        tbody.insertAdjacentHTML('beforeend', rows);
-
-        // @see JENKINS-64504 - Update the sticky buttons position after each search.
-        requestAnimationFrame(() => {
-            layoutUpdateCallback.call()
-        })
-    })
-}
-
-var handleFilter = function (e) {
-    applyFilter(e.target.value)
-};
-
-var debouncedFilter = debounce(handleFilter, 150);
-
-document.addEventListener("DOMContentLoaded", function () {
-    filterInput.addEventListener('input', function (e) {
-        debouncedFilter(e);
-        filterInput.parentElement.classList.add("app-plugin-manager__search--loading");
+    clearOldResults()
+    const rows = pluginManagerAvailable({
+      plugins: plugins.filter(plugin => selectedPlugins.indexOf(plugin.name) === -1),
+      admin
     });
 
-    filterInput.focus();
+    tbody.insertAdjacentHTML('beforeend', rows);
+  })
+}
 
-    applyFilter(filterInput.value);
+const handleFilter = function (e) {
+  applyFilter(e.target.value)
+};
+
+const debouncedFilter = debounce(handleFilter, 150);
+
+document.addEventListener("DOMContentLoaded", function () {
+  searchBar.addEventListener('input', function (e) {
+    debouncedFilter(e);
+    searchBar.parentElement.classList.add("app-plugin-manager__search--loading");
+  });
+
+  searchBar.focus();
+
+  applyFilter(searchBar.value);
 });
-
-registerRefreshServerButton(() => applyFilter(filterInput.value));
