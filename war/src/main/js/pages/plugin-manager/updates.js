668bbea16524e57af420b1acede12e3ffdc4dcc3
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-05-01 17:48:29 +0100

    Cleanup

--- war/src/main/js/pages/plugin-manager/updates.js
+++ war/src/main/js/pages/plugin-manager/updates.js
@@ -17,28 +17,16 @@ searchBar.addEventListener("input", () => {
   });
   const items = document.getElementsBySelector("TR.plugin").concat(document.getElementsBySelector("TR.unavailable"));
 
-  for (let i = 0; i < items.length; i++) {
-    if ((filterParts.length < 1 || filter.length < 2) && items[i].hasClassName("hidden-by-default")) {
-      items[i].addClassName("jenkins-hidden");
+  for (const item of items) {
+    if ((filterParts.length < 1 || filter.length < 2) && item.hasClassName("hidden-by-default")) {
+      item.addClassName("jenkins-hidden");
       continue;
     }
-    let makeVisible = true;
 
-    const pluginId = items[i].getAttribute("data-plugin-id");
-    const content = (items[i].querySelector(".details").innerText + " " + pluginId).toLowerCase();
+    const pluginId = item.getAttribute("data-plugin-id");
+    const content = (item.querySelector(".details").innerText + " " + pluginId).toLowerCase();
+    const hideItem = !filterParts.every(part => content.includes(part));
 
-    for (let j = 0; j < filterParts.length; j++) {
-      const part = filterParts[j];
-      if (content.indexOf(part) < 0) {
-        makeVisible = false;
-        break;
-      }
-    }
-
-    if (makeVisible) {
-      items[i].classList.remove("jenkins-hidden");
-    } else {
-      items[i].classList.add("jenkins-hidden");
-    }
+    item.classList.toggle("jenkins-hidden", hideItem);
   }
 });
