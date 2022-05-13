1b6391c0424ad98a4c6a17070e8904369b7e7a06
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-05-13 22:17:13 +0100

    Update keyboard-shortcuts.js

--- war/src/main/js/keyboard-shortcuts.js
+++ war/src/main/js/keyboard-shortcuts.js
@@ -1,7 +1,5 @@
 import hotkeys from "hotkeys-js"
 
-const ADD_SVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M256 112v288M400 256H112"/></svg>`
-
 window.addEventListener("load", () => {
   hotkeys("*", ({key}) => {
     if (key === "Meta") {
