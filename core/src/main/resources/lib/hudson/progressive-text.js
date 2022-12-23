8ba9f9c32c31d474242ce92a61cec736d53298de
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-12-23 02:00:16 +0000

    Tidy up

--- core/src/main/resources/lib/hudson/progressive-text.js
+++ core/src/main/resources/lib/hudson/progressive-text.js
@@ -3,22 +3,21 @@ Behaviour.specify(
   "progressive-text",
   0,
   function (holder) {
-    let href = holder.getAttribute("data-href");
-    let idref = holder.getAttribute("data-idref");
-    let spinner = holder.getAttribute("data-spinner");
-    let startOffset = holder.getAttribute("data-start-offset");
-    let onFinishEvent = holder.getAttribute("data-on-finish-event");
+    const href = holder.getAttribute("data-href");
+    const idref = holder.getAttribute("data-idref");
+    const spinner = holder.getAttribute("data-spinner");
+    const startOffset = holder.getAttribute("data-start-offset");
+    const onFinishEvent = holder.getAttribute("data-on-finish-event");
 
-    var scroller = new AutoScroller(document.body);
+    const scroller = new AutoScroller(document.body);
+
     /*
-  fetches the latest update from the server
-  @param e
-      DOM node that gets the text appended to
-  @param href
-      Where to retrieve additional text from
-  */
+     * fetches the latest update from the server
+     * @param e DOM node that gets the text appended to
+     * @param href Where to retrieve additional text from
+     */
     function fetchNext(e, href, onFinishEvent) {
-      var headers = {};
+      const headers = {};
       if (e.consoleAnnotator !== undefined) {
         headers["X-ConsoleAnnotator"] = e.consoleAnnotator;
       }
@@ -26,7 +25,7 @@ Behaviour.specify(
       function isBottom() {
         const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
         const distanceFromBottom = scrollHeight - scrollTop - clientHeight;
-        return distanceFromBottom < 30;
+        return distanceFromBottom < 30 && scrollTop !== 0;
       }
 
       new Ajax.Request(href, {
@@ -35,36 +34,35 @@ Behaviour.specify(
         requestHeaders: headers,
         onComplete: function (rsp) {
           /* append text and do autoscroll if applicable */
-          var stickToBottom = isBottom();
-          var text = rsp.responseText;
-          if (text != "") {
-            let index = document.querySelectorAll(".longhorn").length;
-            const lines = text.split("\n");
-            lines.forEach((line) => {
-              if (line.trim() === "") {
-                return;
-              }
+          const stickToBottom = isBottom();
+          const text = rsp.responseText;
 
+          console.log(text.split("asda"))
+
+          if (text !== "") {
+            let index = document.querySelectorAll(".longhorn").length;
+            let lines = text.split("\n");
+            lines = lines.slice(0, -1);
+
+            lines.forEach((line) => {
               index ++;
-              var p = document.createElement("DIV");
+              const p = document.createElement("DIV");
               p.classList.add("longhorn")
-              p.innerHTML = `<a id="${index}" href="#${index}">${index}</a><span>${line}</span>`
+              p.innerHTML = `<span data-line-number="${index}"></span><span>${line}</span>`;
               e.appendChild(p);
               Behaviour.applySubtree(p);
             })
 
             e.style.setProperty('--output-inset', index.toString().length + "ch");
 
-
-
-            // if (stickToBottom) {
-            //   scroller.scrollToBottom();
-            // }
+            if (stickToBottom) {
+              scroller.scrollToBottom();
+            }
           }
 
           e.fetchedBytes = rsp.getResponseHeader("X-Text-Size");
           e.consoleAnnotator = rsp.getResponseHeader("X-ConsoleAnnotator");
-          if (rsp.getResponseHeader("X-More-Data") == "true") {
+          if (rsp.getResponseHeader("X-More-Data") === "true") {
             setTimeout(function () {
               fetchNext(e, href, onFinishEvent);
             }, 1000);
