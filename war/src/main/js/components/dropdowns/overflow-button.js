83ed56bd7af65f76aea6182ef85422b26ac58cab
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-12-25 21:13:27 +0000

    Init

--- war/src/main/js/components/dropdowns/overflow-button.js
+++ war/src/main/js/components/dropdowns/overflow-button.js
@@ -1,4 +1,5 @@
 import Utils from "@/components/dropdowns/utils";
+import behaviorShim from "@/util/behavior-shim";
 
 /**
  * Does something
@@ -6,7 +7,7 @@ import Utils from "@/components/dropdowns/utils";
 function init() {
   behaviorShim.specify(
     "[dropdown]",
-    "-dropdown-",
+    "-dropdown-123-",
     1000,
     (element) => {
       Utils.generateDropdown(element, (instance) => {
