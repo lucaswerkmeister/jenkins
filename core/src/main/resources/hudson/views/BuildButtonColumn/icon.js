7954065c22578b4921a390a86b77fed284658a94
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-12-27 00:16:07 +0000

    Init

--- core/src/main/resources/hudson/views/BuildButtonColumn/icon.js
+++ core/src/main/resources/hudson/views/BuildButtonColumn/icon.js
@@ -1,17 +1,32 @@
 Behaviour.specify(
-  ".build-button-column-icon-reference-holder",
+  "[data-type='build-button']",
   "build-button-column",
   0,
-  function (e) {
-    var url = e.getAttribute("data-url");
-    var message = e.getAttribute("data-notification");
-    var id = e.getAttribute("data-id");
-    var icon = document.getElementById(id);
+  (button) => {
+    button.addEventListener("click", async () => {
+      button.style.pointerEvents = "none";
 
-    icon.onclick = function () {
+      const url = button.dataset.url;
       new Ajax.Request(url);
-      hoverNotification(message, this, -100);
-      return false;
-    };
+
+      const children = Array.from(button.children);
+      const timer = ms => new Promise(res => setTimeout(res, ms));
+
+      for (let i = 0; i < children.length; i++){
+        if (i === 0) {
+          continue;
+        }
+
+        children[i - 1].classList.remove("longhorn-active");
+        children[i].classList.add("longhorn-active");
+
+        await timer(3000);
+      }
+
+      button.style.pointerEvents = "all";
+      children[children.length - 1].classList.remove("longhorn-active");
+      children[0].classList.add("longhorn-active");
+    });
   }
 );
+
