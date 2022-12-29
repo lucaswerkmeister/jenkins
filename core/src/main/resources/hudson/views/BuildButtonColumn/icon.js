efdef7757d91121d5e7adddd7d4a607fb9827f12
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-12-29 23:06:49 +0000

    Tidy up

--- core/src/main/resources/hudson/views/BuildButtonColumn/icon.js
+++ core/src/main/resources/hudson/views/BuildButtonColumn/icon.js
@@ -1,3 +1,11 @@
+function createElementFromHtml(html) {
+  const template = document.createElement("template");
+  template.innerHTML = html.trim();
+  return template.content.firstElementChild;
+}
+
+const CHECK_SYMBOL = `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Checkmark</title><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M416 128L192 384l-96-96"/></svg>`;
+
 Behaviour.specify(
   "[data-type='build-button']",
   "build-button-column",
@@ -5,6 +13,13 @@ Behaviour.specify(
   (button) => {
     button.addEventListener("click", async () => {
       button.style.pointerEvents = "none";
+
+      if (!button.classList.contains("longhorn")) {
+        button.classList.add("longhorn");
+        button.children[0].classList.add("longhorn-active");
+        button.append(createElementFromHtml(`<span class="jenkins-spinner"></span>`))
+        button.append(createElementFromHtml(CHECK_SYMBOL))
+      }
 
       const url = button.dataset.url;
       new Ajax.Request(url);
@@ -14,6 +29,7 @@ Behaviour.specify(
 
       for (let i = 0; i < children.length; i++){
         if (i === 0) {
+          await timer(100);
           continue;
         }
 
