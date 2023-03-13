1dde7809368763fa32ba85fb9ee9cb540deb742e
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2023-03-13 21:59:55 +0000

    Lint

--- war/src/main/js/components/dropdowns/hetero-list.js
+++ war/src/main/js/components/dropdowns/hetero-list.js
@@ -2,7 +2,9 @@ import tippy from "tippy.js";
 import Templates from "@/components/dropdowns/templates";
 import { createElementFromHtml } from "@/util/dom";
 import { registerSortableDragDrop } from "@/sortable-drag-drop";
-import {SEARCH} from "../../util/symbols";
+import { SEARCH } from "../../util/symbols";
+
+const MINIMUM_FILTER_OPTIONS_FOR_FILTER = 5;
 
 /*
  * Generates a jump list for the active breadcrumb to jump to
@@ -17,7 +19,9 @@ function init() {
 
     // Templates for hetero-lists are stored in a div on load (.prototypes), so we
     // need to take them and translate them into template objects we can use for our dropdown menu
-    const prototypes = button.closest(".hetero-list-container").querySelector(".prototypes");
+    const prototypes = button
+      .closest(".hetero-list-container")
+      .querySelector(".prototypes");
     const insertionPoint = prototypes.previousSibling;
 
     // Initialize drag and drop
@@ -39,8 +43,8 @@ function init() {
 
     if (templates.length === 1) {
       button.addEventListener("click", () => {
-        clickItem(templates[0], insertionPoint, supportsDragDrop);
-      })
+        itemClickEvent(templates[0], insertionPoint, supportsDragDrop);
+      });
       return;
     }
 
@@ -49,32 +53,13 @@ function init() {
     // Generate a list of menu items for the dropdown to use
     let menuItems = document.createElement("div");
     menuItems.classList.add("jenkins-dropdown");
-    if (templates.length > 0) {
-      const filterBar = createElementFromHtml(
-        `<div class="jenkins-dropdown__filter">
-            <input class="jenkins-search__input" type="search" placeholder="Filter">
-            ${SEARCH}
-          </div>
-      `);
-      const itemsPlaceholder = createElementFromHtml(
-        `<p class="jenkins-dropdown__placeholder" style="display: none">No items</p>`
-      )
-      const filterBarInput = filterBar.querySelector("input");
-      menuItems.append(filterBar);
-      menuItems.append(itemsPlaceholder);
 
-      filterBarInput.addEventListener("input", () => {
-        const itemList = filterBarInput.parentElement.parentElement;
-        itemsPlaceholder.style.display = "";
-        for (const item of itemList.querySelectorAll(".jenkins-dropdown__item")) {
-          const match = item.innerText.toLowerCase().includes(filterBarInput.value.toLowerCase());
-          item.style.display = match ? "" : "none";
-          if (match) {
-            itemsPlaceholder.style.display = "none";
-          }
-        }
-      })
+    // Add a filter bar if there are more than N items
+    if (templates.length >= MINIMUM_FILTER_OPTIONS_FOR_FILTER) {
+      generateFilterBar(menuItems);
     }
+
+    // Map the templates and add the items to the dropdown
     menuItems.append(
       ...templates.map((templateItem) => {
         const menuItem =
@@ -82,7 +67,9 @@ function init() {
                         ${templateItem.title}
                       </button>`);
 
-        menuItem.addEventListener("click", () => clickItem(templateItem, insertionPoint, supportsDragDrop));
+        menuItem.addEventListener("click", () =>
+          itemClickEvent(templateItem, insertionPoint, supportsDragDrop)
+        );
 
         return menuItem;
       })
@@ -96,7 +83,7 @@ function init() {
         const filterBar = instance.popper.querySelector("input");
         if (filterBar) {
           filterBar.value = "";
-          filterBar.dispatchEvent(new Event('input', { bubbles: true }));
+          filterBar.dispatchEvent(new Event("input", { bubbles: true }));
         }
         instance.popper.addEventListener("click", () => {
           instance.hide();
@@ -107,15 +94,15 @@ function init() {
         if (filterBar) {
           filterBar.focus();
 
-          // console.log()
+          // Set a minimum width on the dropdown so that it doesn't resize when filtering
           instance.popper.style.minWidth = instance.popper.offsetWidth + "px";
         }
-      }
+      },
     });
   });
 }
 
-function clickItem(templateItem, insertionPoint, supportsDragDrop) {
+function itemClickEvent(templateItem, insertionPoint, supportsDragDrop) {
   const card = document.createElement("div");
   card.className = "repeated-chunk";
   card.setAttribute("name", templateItem.name);
@@ -124,10 +111,7 @@ function clickItem(templateItem, insertionPoint, supportsDragDrop) {
 
   insertionPoint.parentElement.insertBefore(card, insertionPoint);
 
-  renderOnDemand(
-    findElementsBySelector(card, "div.config-page")[0],
-    () => {}
-  );
+  renderOnDemand(card.querySelector("div.config-page"), () => {}, false);
 
   Behaviour.applySubtree(card, true);
   ensureVisible(card);
@@ -136,6 +120,36 @@ function clickItem(templateItem, insertionPoint, supportsDragDrop) {
   if (supportsDragDrop) {
     registerSortableDragDrop(insertionPoint.parentElement);
   }
+}
+
+function generateFilterBar(container) {
+  const filterBar = createElementFromHtml(
+    `<div class="jenkins-dropdown__filter">
+            <input class="jenkins-search__input" type="search" placeholder="Filter">
+            ${SEARCH}
+          </div>
+      `
+  );
+  const itemsPlaceholder = createElementFromHtml(
+    `<p class="jenkins-dropdown__placeholder" style="display: none">No items</p>`
+  );
+  const filterBarInput = filterBar.querySelector("input");
+  container.append(filterBar);
+  container.append(itemsPlaceholder);
+
+  filterBarInput.addEventListener("input", () => {
+    const itemList = filterBarInput.parentElement.parentElement;
+    itemsPlaceholder.style.display = "";
+    for (const item of itemList.querySelectorAll(".jenkins-dropdown__item")) {
+      const match = item.innerText
+        .toLowerCase()
+        .includes(filterBarInput.value.toLowerCase());
+      item.style.display = match ? "" : "none";
+      if (match) {
+        itemsPlaceholder.style.display = "none";
+      }
+    }
+  });
 }
 
 export default { init };
