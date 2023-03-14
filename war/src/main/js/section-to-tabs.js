08c2c05fe2caa3a879dc93dbf0453b7d55809b41
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2023-03-14 21:27:25 +0000

    Init

--- war/src/main/js/section-to-tabs.js
+++ war/src/main/js/section-to-tabs.js
@@ -11,38 +11,52 @@ tabPanes.forEach((tabPane) => {
 tabPanes[0].style.display = "block";
 
 const tabBar = document.createElement("div");
-tabBar.className = "tabBar";
+tabBar.className = "jenkins-tabs";
+const tabHighlight = document.createElement("div");
+tabHighlight.className = "jenkins-tabs__highlight";
+tabBar.append(tabHighlight);
 
 // Add tabs for each tab pane
 tabPanes.forEach((tabPane, index) => {
   const tabPaneTitle = tabPane.querySelector(".jenkins-tab-pane__title");
   tabPaneTitle.style.display = "none";
 
-  const tab = document.createElement("div");
-  tab.className = "tab";
+  const tab = document.createElement("button");
+  tab.className = "jenkins-tabs__tab";
+  tab.innerText = tabPaneTitle.textContent;
 
   if (index === 0) {
-    tab.classList.add("active");
+    tab.classList.add("jenkins-tabs__tab--selected");
   }
 
   tab.addEventListener("click", function () {
-    document.querySelectorAll(".tab").forEach((tab) => {
-      tab.classList.remove("active");
+    document.querySelectorAll(".jenkins-tabs__tab").forEach((tab) => {
+      tab.classList.remove("jenkins-tabs__tab--selected");
     });
-    tab.classList.add("active");
+    tab.classList.add("jenkins-tabs__tab--selected");
 
     tabPanes.forEach((tabPane) => {
       tabPane.style.display = "none";
     });
     tabPanes[index].style.display = "block";
+
+    tabHighlight.style.top = "2px";
+    tabHighlight.style.bottom = "2px";
+    tabHighlight.style.width = tab.offsetWidth + "px";
+    tabHighlight.style.left = tab.offsetLeft + "px";
+
+    setTimeout(() => {
+      tabHighlight.style.top = "0";
+      tabHighlight.style.bottom = "0";
+    }, 200);
   });
 
-  const tabLink = document.createElement("a");
-  tabLink.innerText = tabPaneTitle.textContent;
-
-  tab.append(tabLink);
-
   tabBar.append(tab);
+
+  console.log(tab.offsetWidth)
+
 });
 
 content.insertBefore(tabBar, tabPanes[0]);
+
+tabBar.querySelector(".jenkins-tabs__tab").click();
