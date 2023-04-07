d4a08fb9e1f6447cbcaa29e5137173c5fe10989a
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2023-04-07 12:10:49 +0100

    Revert files

--- war/src/main/js/pages/plugins/installed.js
+++ war/src/main/js/pages/plugins/installed.js
@@ -1,6 +1,4 @@
 import { notificationBar } from '@/components/notifications';
-import * as Symbols from "@/util/symbols";
-import { createElementFromHtml } from "@/util/dom";
 
 // Enable/disables plugins
 const toggleSwitches = [...document.querySelectorAll(".jenkins-table .jenkins-toggle-switch input")];
@@ -23,43 +21,12 @@ function togglePluginState(toggleSwitch) {
   updateRestartNotificationVisibility();
 }
 
-function showRestartNotification() {
-  // Don't create another notification if one already exists
-  if (document.querySelector(".jenkins-notification--visible")) {
-    return;
-  }
-
-  let notificationContent;
-
-  if (getJelloAttribute('can-restart') === 'true') {
-    notificationContent = createElementFromHtml(`
-        <form method="post" action="safeRestart">
-            <button class="jenkins-button jenkins-!-inherit-color jenkins-!-margin-top-2">
-                Restart once no jobs are running
-            </button>
-        </form>
-    `);
-  }
-
-  notificationBar.show('Changes will take effect when you restart Jenkins', {
-    alertClass: "jenkins-notification jenkins-notification--warning",
-    icon: Symbols.WARNING,
-    persistent: true,
-    content: notificationContent,
-    maxWidth: "335px"
-  });
-}
-
 function updateRestartNotificationVisibility() {
   // Has anything changed since its original state?
   const stateChanged = toggleSwitches.find((e) => e.checked.toString() !== e.getAttribute('original'));
   const requiresRestart = getJelloAttribute('restart-required') === 'true';
 
-  if (stateChanged || requiresRestart) {
-    showRestartNotification();
-  } else {
-    notificationBar.hide();
-  }
+  document.querySelector("#needRestart").style.display = (stateChanged || requiresRestart) ? 'block' : 'none';
 }
 
 updateRestartNotificationVisibility();
