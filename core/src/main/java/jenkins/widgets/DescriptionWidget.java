1132d10c2116cfbc624c894ac8691b8f8fb8a881
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2023-05-19 00:02:02 +0100

    Init

---
+++ core/src/main/java/jenkins/widgets/DescriptionWidget.java
@@ -0,0 +1,10 @@
+package jenkins.widgets;
+
+import hudson.Extension;
+import hudson.widgets.Widget;
+import org.jenkinsci.Symbol;
+
+@Extension(ordinal = 200) @Symbol("description")
+public class DescriptionWidget extends Widget {
+
+}
