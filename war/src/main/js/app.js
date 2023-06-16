ef760c9982c56c15ffc911ab0e8a3e88b71e6ac7
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2023-06-16 22:18:51 +0100

    Init

--- war/src/main/js/app.js
+++ war/src/main/js/app.js
@@ -7,3 +7,17 @@ Dropdowns.init();
 Notifications.init();
 SearchBar.init();
 Tooltips.init();
+
+// const url = "jenkins/job/Scalemate/job/feat%252Fpi-3125-add-stuff/buildHistory/ajax";
+const url = "buildHistory/ajax";
+const newbuildhistory = document.querySelector(".newbuildhistory");
+
+fetch(url, {
+  headers: {
+    // n: buildHistoryContainer.headers[1],
+  },
+}).then((rsp) => {
+  rsp.text().then((responseText) => {
+    newbuildhistory.innerHTML = "<div class='buildsss'>Builds</div>" + responseText;
+  })
+});
